78d27123b37351c788a64222419a6d053981b593
wtz <wtz@sh.com>
2024-07-18 16:59:53 +0800

    1. 我也不知道修改了啥

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="relative h-screen w-screen overflow-auto">
-    <img :src="bgimgSource" alt="Background Image" class="absolute inset-0 object-cover h-full w-full z-0" />
-    <div class="absolute inset-0 flex flex-col justify-center items-center pt-6 ">
-      <div class=" z-10 mx-5 max-w-full max-h-full overflow-auto">
-        <img ref="imageRef" :src="imageUrl" alt="" class="max-h-full w-full">
+    <img :src="bgimgSource" class="w-screen h-svh fixed inset-0" />
 
-        <div class="mt-8  h-10 save-image inline-block py-2 px-4 border border-black rounded-full text-gray-800">长按图片即可保存</div>
-
-      </div>
-    </div>
+    <img
+      :src="bgimgSource"
+      alt="Background Image"
+      class="absolute inset-0 object-cover h-full w-full z-0"
+    />
   </div>
 </template>
 
 <script setup>
-import { ref , computed,onMounted} from 'vue'
-import bgimgSource from "@assets/bg.png"
+import { ref, computed, onMounted } from "vue";
+import bgimgSource from "@assets/bg.png";
 
 defineProps({
   msg: String,
-})
+});
 const canvasRef = ref(null);
-const imageBlob = ref(null)
+const imageBlob = ref(null);
 
-const imageRef = ref(null)
-var imageData = null
+const imageRef = ref(null);
+var imageData = null;
 const imageUrl = computed(() => {
   const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get('a') || '';
+  return urlParams.get("a") || "";
 });
-
 </script>
 
 <style>
