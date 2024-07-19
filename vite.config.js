481e1a2a40722efff6f619305beef156d3ca0844
wtz <wtz@sh.com>
2024-07-19 15:30:31 +0800

    add: 子路径示例

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 const resolve = (dir) => path.join(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/test/", // 设置子路径 //打包时部署的网站子路径
   plugins: [vue()],
-  resolve:  {
+  resolve: {
     alias: {
-      "@":resolve('src'),
-      "@assets":resolve('src/assets'),
-      "@components":resolve('src/components')
-    }
-    
-},
-server: {
-  host: '0.0.0.0', // 或者你的本地 IP 地址
-}
-})
+      "@": resolve("src"),
+      "@assets": resolve("src/assets"),
+      "@components": resolve("src/components"),
+    },
+  },
+  server: {
+    host: "0.0.0.0", // 或者你的本地 IP 地址
+  },
+});
